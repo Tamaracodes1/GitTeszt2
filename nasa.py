beb64a1291dee74e223d4dc65d7dac11a037ea94
ktami <ktami5959@gmail.com>
2024-02-07 15:33:44 +0100

    third commit

--- nasa.py
+++ nasa.py
@@ -1,35 +1,36 @@
 def file_readin():
     with open('astronauts.csv', 'r', encoding='utf-8') as astronauts:
         astronauts.readline()
-        imp_data = []
+        important_data = []
         for line in astronauts:
             data = line.strip().split(',')
             month, day, year = data[4].split('/')
-            imp_data.append(month)
-        return imp_data
+            important_data.append(month)
+        return important_data
 
 
 def birth_date(month_list):
-    res = [eval(i) for i in month_list]
-    x = 1
+    new_month_list = [eval(i) for i in month_list]
+    numerator = 1
     how_many = []
     for i in range(12):
-        count = res.count(x)
+        count = new_month_list.count(numerator)
         how_many.append(count)
-        x += 1
+        numerator += 1
     return how_many
 
 
 def percent(dates):
     hightest = sorted(dates)[-3:]
-    x = 2
-    y = 1
+    numerator_1 = 2
+    numerator_2 = 1
     for i in range(3):
-        number = dates.index(hightest[x]) + 1
-        print(f'A(z) {number}. hónapban {hightest[x]}-en születtek, ezzel ez a {y}. legtöbb születésű hónap'
-              f' az asztronauták között, ez az összes szüetésnek a(z) {round(hightest[x]/sum(dates)*100, 1)}%-a.')
-        x -= 1
-        y += 1
+        number = dates.index(hightest[numerator_1]) + 1
+        print(f'A(z) {number}. hónapban {hightest[numerator_1]}-en születtek, ezzel ez a {numerator_2}. legtöbb '
+              f'születésű hónap az asztronauták között, ez az összes szüetésnek a(z) '
+              f'{round(hightest[numerator_1] / sum(dates) * 100, 1)}%-a.')
+        numerator_1 -= 1
+        numerator_2 += 1
 
 
 def main():
